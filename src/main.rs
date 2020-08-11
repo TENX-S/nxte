578f5e2c101c22c9f453d1d262cf6cd4cbb9abcf
TENX-S <ttenx@pm.me>
2020-08-11 18:41:21 +0800

    * Key press poll * Display partial input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+extern crate termion;
+
+
+use std::io::{ Write, stdout, stdin };
+use termion::{ event::Key, input::TermRead, raw::IntoRawMode };
+
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let stdin = stdin();
+
+    let mut stdout = stdout().into_raw_mode().unwrap();
+    writeln!(stdout, "{}{}{}", termion::clear::All, termion::cursor::Goto(1, 1), termion::cursor::Hide).unwrap();
+
+    for c in stdin.keys() {
+
+        write!(stdout, "{}{}", termion::cursor::Goto(1, 1), termion::clear::CurrentLine).unwrap();
+
+        match c.unwrap() {
+            // Exit.
+            Key::Char('q') => break,
+            Key::Char(c)   => println!("{}", c),
+            Key::Alt(c)    => println!("Alt-{}", c),
+            Key::Ctrl(c)   => println!("Ctrl-{}", c),
+            Key::Left      => println!("<left>"),
+            Key::Right     => println!("<right>"),
+            Key::Up        => println!("<up>"),
+            Key::Down      => println!("<down>"),
+            _              => println!("Other"),
+        }
+
+        stdout.flush().unwrap();
+    }
+
+    write!(stdout, "{}", termion::cursor::Show).unwrap();
 }
